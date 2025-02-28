6bb9417f1c0e9f7140bf732cd55a3d025ebf13b2
Akila Ramesh Dasari <dasariakilaramesh13@gmail.com>
2025-02-28 14:48:11 -0500

    Using basic web scraper functionality to get the title of a website(https://techcrunch.com/)

--- src/main/java/org/multithreading/WebScraper.java
+++ src/main/java/org/multithreading/WebScraper.java
@@ -1,7 +1,20 @@
 package org.multithreading;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class WebScraper {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String url = "https://techcrunch.com/";
+
+        try{
+            Document techCrunchDoc = Jsoup.connect(url).get();
+            System.out.println("Title: " + techCrunchDoc.title());
+        }
+        catch(IOException ioexception){
+            ioexception.printStackTrace();
+        }
     }
 }
